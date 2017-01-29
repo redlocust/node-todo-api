deaac8a638b1690d64f525acb58d9d5dbd29bbf3
alexei Kuznecov <alexei Kuznecov>
2017-01-29 13:10:25 +0300

    add test for GET /todos

--- server/server.js
+++ server/server.js
@@ -27,8 +27,8 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then(
-        (docs) => {
-            res.send({docs});
+        (todos) => {
+            res.send({todos});
         },
         (e) => {
             res.statusCode(400).send(e);
